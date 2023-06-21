faacc577d49dc1373682404c82b2078716beec78
appan-abi-ai <137161410+appan-abi-ai@users.noreply.github.com>
2023-06-21 01:51:32 -0700

    Update age 2.py

--- age 2.py
+++ age 2.py
@@ -2,4 +2,4 @@ Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> Weight=47
 >>> print(Weight)
-45
+47

5ee9874188e1eaac14ff69c24226b27ebdf6e595
appan-abi-ai <137161410+appan-abi-ai@users.noreply.github.com>
2023-06-21 01:52:44 -0700

    Update age 3.py

--- age 3.py
+++ age 3.py
@@ -2,4 +2,5 @@ Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> Height=160
 >>> print(Height)
-149
+160
+  
